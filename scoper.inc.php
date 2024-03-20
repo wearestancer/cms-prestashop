6e5c763af4c367fefc806b2c20bc07c8466c84c6
Sebastien <sderen@stancer.com>
2024-03-20 14:41:22 +0100

    adapting WC scoper to prestashop

---
+++ scoper.inc.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Config file for php-scoper to prefix all our prod dependency (for now only stancer and psr).
+ *
+ * For more Information on Scopping see:
+ * https://github.com/humbug/php-scoper/blob/master/docs/configuration.md
+ *
+ * @since unreleased
+ * @see https://www.stancer.com/
+ *
+ * @license MIT
+ */
+
+declare(strict_types=1);
+
+use Isolated\Symfony\Component\Finder\Finder;
+
+return [
+    'prefix' => 'Stancer\\Scoped\\Isolated',
+    'output-dir' => 'vendor-prefixer',
+    'finders' => [
+        Finder::create()->files()->in('./vendor/'),
+        Finder::create()->append(['./composer.json']),
+    ],
+];
