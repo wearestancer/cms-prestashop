3c25acdb8183fe202b4a6dd7608a80c5e866dd8e
Joel <jdasilva@stancer.com>
2024-03-29 17:16:19 +0100

    Refact devcontainer

--- .devcontainer/scripts/post-install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-echo "\n* Install Stancer module...";
-runuser -g www-data -u www-data -- php -d memory_limit=-1 bin/console prestashop:module install stancer
-
-echo "\n* Clearing cache...";
-runuser -g www-data -u www-data -- php -d memory_limit=-1 bin/console cache:clear
--- .devcontainer/ssl/renew-certs.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/sh
-
-openssl req -config /etc/ssl/localhost.cnf -new -x509 -sha256 -newkey rsa:4096 -nodes -days 30 -keyout /etc/apache2/ssl/localhost.key -out /etc/apache2/ssl/localhost.crt
