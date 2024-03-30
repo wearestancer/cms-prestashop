9fcc79d878e0067828bf3272580aa66b52eefa6e
Joel <jdasilva@stancer.com>
2024-03-30 22:48:19 +0100

    Update dependencies

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -1,13 +1,11 @@
 import gulp from 'gulp';
 import autoprefixer from 'autoprefixer';
 import cssnano from 'cssnano';
-import gulpif from 'gulp-if';
 import gulpSass from 'gulp-sass';
 import postcss from 'gulp-postcss';
-import sourcemaps from 'gulp-sourcemaps';
 import uglify from 'gulp-uglify';
 import pump from 'pump';
-import sassCompiler from 'sass';
+import * as sassCompiler from 'sass';
 
 const sass = gulpSass(sassCompiler);
 const paths = {
@@ -24,18 +22,9 @@ const uglifyConf = {
   mangle: false,
 };
 
-let maps = true;
-
-const noMap = (cb) => {
-  maps = false;
-
-  cb();
-};
-
 export const styles = () => {
   return gulp
     .src(paths.styles.src)
-    .pipe(gulpif(maps, sourcemaps.init()))
     .pipe(sass().on('error', sass.logError))
     .pipe(
       postcss([
@@ -43,7 +32,6 @@ export const styles = () => {
         cssnano(),
       ]),
     )
-    .pipe(gulpif(maps, sourcemaps.write('.')))
     .pipe(gulp.dest(paths.styles.dest));
 };
 
@@ -51,16 +39,14 @@ export const scripts = (cb) => {
   pump(
     [
       gulp.src(paths.scripts.src),
-      gulpif(maps, sourcemaps.init()),
       uglify(uglifyConf),
-      gulpif(maps, sourcemaps.write('.')),
       gulp.dest(paths.scripts.dest),
     ],
     cb,
   );
 };
 
-export const build = gulp.series(noMap, gulp.parallel(styles, scripts));
+export const build = gulp.parallel(styles, scripts);
 
 export const watch = () => {
   gulp.watch(paths.styles.src, styles);
