fe985e147fc0d9a1631b21d6d716296a9e3b2a24
Joel <jdasilva@stancer.com>
2024-04-02 17:50:39 +0200

    More precise CS fixer configuration

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -2,10 +2,16 @@
 
 $config = new PrestaShop\CodingStandards\CsFixer\Config();
 
-/** @var Symfony\Component\Finder\Finder $finder */
-$finder = $config->setUsingCache(true)->getFinder();
-$finder->in(__DIR__)->exclude('vendor');
+$config->setUsingCache(true)->setCacheFile(__DIR__ . '/vendor/.cache/php-cs-fixer.cache');
 
-$config->setCacheFile(__DIR__ . '/.cache/php-cs-fixer.cache');
+/** @var Symfony\Component\Finder\Finder $finder */
+$finder = $config->getFinder();
+$finder->in(__DIR__)
+    ->exclude('.devcontainer')
+    ->exclude('.git')
+    ->exclude('node_modules')
+    ->exclude('translations')
+    ->exclude('vendor')
+;
 
 return $config;
