36c4b2da43320553bb59d84d9b14a93586e5a6e1
Joel <jdasilva@stancer.com>
2024-04-17 17:02:29 +0000

    Adapt CI

--- scripts/git-filters.sh
+++ scripts/git-filters.sh
@@ -4,6 +4,7 @@ set -eu
 
 scripts_dir=$(dirname $(realpath "$0"))
 main_dir=$(dirname "$scripts_dir")
+apply="${1:-}"
 
 
 find "$scripts_dir" -name 'smudge.sh' \
@@ -11,3 +12,11 @@ find "$scripts_dir" -name 'smudge.sh' \
 
 find "$scripts_dir" -name 'clean.sh' \
   | awk -F/ '{ name = NF - 1; print "git config filter." $name ".clean ./scripts/" $name "/" $NF }' | sh
+
+
+if [ -n "$apply" ]; then
+  grep 'filter=' "${main_dir}/.gitattributes" | awk '{ print $1 }' \
+    | xargs -I % find "$main_dir" -name '%' -delete
+
+  git restore .
+fi
