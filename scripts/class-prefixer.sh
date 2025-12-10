ea614435e2fc3bbcf7fec6c740c996bb77b9e8ee
Sebastien <sderen@stancer.com>
2025-12-10 12:58:39 +0000

    CHANGE: scoper inc

--- scripts/class-prefixer.sh
+++ scripts/class-prefixer.sh
@@ -2,16 +2,16 @@
 
 set -eu
 
-# current_dir=$(dirname $(realpath $0))
-# search_dir=$(dirname "$current_dir")
-# # We find all the files with a dependancy to Stancer.
-# files=$(grep -REl '^use Stancer(;| as StancerSDK;)$' --exclude-dir=node_modules/ --exclude-dir=vendor/ "$search_dir")
+current_dir=$(dirname $(realpath $0))
+search_dir=$(dirname "$current_dir")
+# We find all the files with a dependancy to Stancer.
+files=$(grep -REl '^use Psr' "$search_dir/vendor/stancer")
 
-# # We create a new file for our archive with the dependency scoped
-# for file in $files; do
-#   sed -i'.old' -re 's/use Stancer\b/use Stancer\\Scoped\\Isolated\\Stancer/g' "$file"
-# done
+# We create a new file for our archive with the dependency scoped
+for file in $files; do
+  sed -i'.old' -re 's/use Psr\b/use Stancer\\Scoped\\Isolated\\Psr/g' "$file"
+done
 
-rm -rf vendor
-mkdir vendor
-mv build/vendor/* vendor
+rm -rf vendor/psr
+mv build/* vendor
+rm -rf build
